ea60cc56b99b2b3bf83b585cd5fe59ca89b781ba
Gonçalo Grilo <grilogoncalo31@ua.pt>
2016-11-30 10:04:41 +0000

    mudar background color da segunda parte

--- inde.js
+++ inde.js
@@ -1,6 +1,6 @@
 var first = ["Caros colegas,", "Por outro lado,", "Não podemos esquecer que", "Do mesmo modo,", "A prática mostra que", "Nunca é demais insistir que", "A experiência mostra que", "É fundamental ressaltar que", "O incentivo ao avanço tecnológico, assim como", "Assim mesmo,"];
 
-var second = ["a execução deste projecto", "a complexidade dos estudos efectuados", "a actual estrutura de organização", "o novo modelo estrutural aqui preconizado", "o desenvolvimento de formas distintas de atuação", "a constante divulgação das informações", "a consolidação das estruturas", "a análise dos diversos resultados", "o ínicio do programa de formação de atitudes", "a expansão da nossa atividade"];
+var second = ["a execução deste projecto", "a complexidade dos estudos efectuados", "a actual estrutura da Universidade", "o novo modelo estrutural aqui preconizado", "o desenvolvimento de formas distintas de atuação", "a constante divulgação das informações", "a consolidação das estruturas", "a análise dos diversos resultados", "o ínicio do programa de formação de atitudes", "a expansão da nossa atividade"];
 
 var third = ["obriga à análise", "cumpre um papel essencial na formulação", "auxilia a preparação e a estruturação", "contribui para a correcta determinação", "assume importantes posições na definição", "facilita a definição", "prejudica a percepção da importância", "oferece uma boa oportunidade de verificação", "acarreta um processo de reformulação", "exige precisão e definição"];
 
@@ -9,45 +9,14 @@ var fourth = ["das nossas opções de desenvolvimento futuro.", "das nossas meta
 //var filtros = "grayscale(100%);", "blur(5px);", "invert(100%)", "hue-rotate(180deg)", "sepia(80%)"
 var tope = ["Ser UA", "Uma Academia", "Academia em pratica", "Um Rumo Melhor", "Mudança Consciente", "Navegar Além", "Academia grande, outra vez"];
 $(document).ready(function() {
-	/*
-    $('#pagepiling').pagepiling({
-        menu: null,
-        direction: 'vertical',
-        verticalCentered: true,
-        sectionsColor: [],
-        anchors: [],
-        scrollingSpeed: 700,
-        easing: 'swing',
-        loopBottom: false,
-        loopTop: false,
-        css3: true,
-        navigation: {
-            'textColor': '#000',
-            'bulletsColor': '#000',
-            'position': 'right',
-            'tooltips': ['section1', 'section2', 'section3', 'section4']
-        },
-        normalScrollElements: null,
-        normalScrollElementTouchThreshold: 5,
-        touchSensitivity: 5,
-        keyboardScrolling: true,
-        sectionSelector: '.section',
-        animateAnchor: false,
-
-        //events
-        onLeave: function(index, nextIndex, direction){},
-        afterLoad: function(anchorLink, index){},
-        afterRender: function(){},
-    });
-    */
-    $(document).ready(function() {
+	
     	$('#pagepiling').pagepiling(
     	{
     		anchors: ['imagem', 'info']
     	}
 
     	);
-	});
+	
 	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	$('#nomeLista').text("Lista " + possible[Math.floor(Math.random() * possible.length)]);
 	$('#tretaGrande').text(tope[Math.floor(Math.random() * tope.length)]);
@@ -60,5 +29,6 @@ $(document).ready(function() {
 	$('#primeiro').css('box-shadow', 'inset 0 0 0 1000px rgba(' + r + ',' + g +',' + b +',.5)');
     var frase = first[Math.floor(Math.random() * first.length)] + " " + second[Math.floor(Math.random() * second.length)] + " " + third[Math.floor(Math.random() * third.length)] + " " + fourth[Math.floor(Math.random() * fourth.length)];
     $('#tretaLonga').text(frase);
-    $('segundo').css('background-color', 'rgba(' + r + ',' + g +',' + b +',.5)');
+    $('#segundo').css('background-color', 'rgba(' + r + ',' + g +',' + b +',.5)');
+    //$('body').css('background-color', 'rgba(' + r + ',' + g +',' + b +',.5)');
 });
